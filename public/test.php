0e0bbe44259620697247634e21843321608fb581
Jessica Mauerhan <jessicamauerhan@gmail.com>
2018-09-19 10:45:16 -0700

    Fixed timestamp issue on the test file, added new Class and test

--- public/test.php
+++ public/test.php
@@ -17,7 +17,7 @@ $obj   = (object)[
 $id        = $obj->data->id;
 $text      = $obj->data->attributes->text;
 $author    = $obj->data->attributes->author;
-$createdAt = (new DateTime())->format('Y-m-d H:i:s+e');
+$createdAt = (new DateTime())->format('Y-m-d H:i:s');
 
 //Save it
 $dsn    = 'pgsql:dbname=chirper;host=db';
@@ -25,11 +25,13 @@ $dbUser = 'postgres';
 $dbPass = 'postgres';
 $pdo    = new PDO($dsn, $dbUser, $dbPass);
 
-//$sql          = "INSERT INTO chirp(id, chirp_text, author, created_at)
-//VALUES(:id, :chirp_text, :author, :created_at)";
-//$values       = ['id' => $id, 'chirp_text' => $text, 'author' => $author, 'created_at' => $createdAt];
-//$preparedStmt = $pdo->prepare($sql);
-//$result       = $preparedStmt->execute($values);
+$sql          = "INSERT INTO chirp(id, chirp_text, author, created_at)
+VALUES(:id, :chirp_text, :author, :created_at)";
+$values       = ['id' => $id, 'chirp_text' => $text, 'author' => $author, 'created_at' => $createdAt];
+$preparedStmt = $pdo->prepare($sql);
+$result       = $preparedStmt->execute($values);
+var_dump($preparedStmt->errorInfo());
+var_dump($result);
 
 //Get the chirps out to see
 $sql    = "SELECT * FROM chirp ORDER BY created_at DESC";
---
+++ tests/Unit/Chirp/CreateActionTest.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Test\Unit\Chirp;
+
+use Chirper\Chirp\CreateAction;
+use Test\TestCase;
+
+class CreateActionTest extends TestCase
+{
+
+}
